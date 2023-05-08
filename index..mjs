3446334758980a0c11e8e092bc2836dee0d13ca9
RandyYanish <rtaylorfargo@gmail.com>
2023-05-08 15:47:58 -0700

    update to add prompts and tests

--- index..mjs
+++ index..mjs
@@ -5,18 +5,37 @@ import { Circle, Rectangle, Triangle } from './lib/shapes.mjs';
 const prompts = [
     {
         type: 'input',
-        message: 'message',
-        name: 'input'
+        message: 'What would you like to call this file?',
+        name: 'fileName'
     },
     {
-        type: 'password',
-        message: 'What is your password?',
-        name: 'password'
+        type: 'input',
+        message: 'What text would you like to display on the logo? (Max 3 characters)',
+        name: 'logoText'
+    },
+    {
+        type: 'ipnut',
+        message: 'What color would you like your text to be? (keyword or hexadecimal number)',
+        name: 'textColorChoice'
+    },
+    {
+        type: 'list',
+        message: 'What shape would you like the logo to be?',
+        choices: ['Circle', 'Rectangle', 'Triangle'],
+        name: 'shapeChoice'
+    },
+    {
+        type: 'input',
+        message: 'What color would you like your shape to be? (keyword or hexadecimal)',
+        name: 'shapeColorChoice'
     }
 ];
 
-function writeToFile(name, svgData) {
-    fs.writeFile(name + 'svg', svgData, (err) => {
+function writeToFile(fileName, svgData) {
+    let svgData = '';
+    svgData = ``
+
+    fs.writeFile(fileName + '.svg', svgData, (err) => {
         if (err) throw (err);
         console.log('Responses were written to new html file');
     });
